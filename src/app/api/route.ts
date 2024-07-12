61569856f895f23af90e05f38688427803956032
Roberto Robles Beltran <robertomac@MacBook-Pro-de-Roberto.local>
2024-07-12 15:31:49 -0500

    Despliegue a PRD V1.0 eliminacion de secreto

---
+++ src/app/api/route.ts
@@ -0,0 +1,70 @@
+import { NextRequest } from "next/server"
+import OpenAI from "openai"
+import { MessageContentText } from "openai/resources/beta/threads/index.mjs"
+
+const assistantId = "asst_QUh3V3TzbQB1bxf9b11Xnp4U"
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+async function waitForResult(threadId: string, runId: string):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        async function verifyStatus() {
+            try {
+                const currentRun = await openai.beta.threads.runs.retrieve(threadId, runId)
+                if (currentRun.status === "completed") {
+                    resolve()
+                } else {
+                    console.log(currentRun.status)
+                    setTimeout(verifyStatus, 1000)
+                }
+
+            } catch(e) {
+                reject(e)
+            }
+        }
+
+        verifyStatus()
+
+    })
+}
+
+
+export async function GET(request: NextRequest) {
+
+    const params = request.nextUrl.searchParams
+    const textToSearch = params.get("search")
+    let result: string = "";
+
+    if (textToSearch === null ) {
+        return Response.json({data: result})
+    }
+
+    try {
+        const assistant = await openai.beta.assistants.retrieve(assistantId)
+        const thread = await openai.beta.threads.create()
+        await openai.beta.threads.messages.create(thread.id, {
+            role: "user",
+            content: textToSearch
+        })
+        const run = await openai.beta.threads.runs.create(thread.id, {
+            assistant_id: assistant.id
+        })
+
+        await waitForResult(thread.id, run.id)
+
+        const messages = await openai.beta.threads.messages.list(thread.id)
+        messages.data.forEach(m => {
+            if (m.role === "assistant") {
+                const content: MessageContentText[] = m.content as MessageContentText[]
+                result = content[0].text.value
+            }
+        })
+        
+    } catch(e) {
+        console.error("No se pudo obtener el asistente" , e)
+    }
+
+    return Response.json({data: result})
+
+}   
---
+++ src/app/components/Form.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from 'react';
+
+const Form = () => {
+  const [tono, setTono] = useState('')
+  const [topic, setTopic] = useState('')
+  const [result, setResult] = useState('')
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const getData = async () => {
+    const data = await fetch(
+      `http://localhost:3000/api?search=${topic.replace(" ","+")}+en+tono+${tono}`
+    )
+    return data.json()
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if( tono === "" || topic === "") {
+      alert("Por favor llene todos los campos")
+    } else {
+      setResult('')
+      setLoading(true)
+      e.preventDefault()
+      getData().then(r  => {
+        setResult(r.data)
+        setLoading(false)
+        setTopic('')
+        setTono('')
+      })
+    }
+  }
+ 
+  return (
+      <>
+        <div className="form-section container flex justify-center">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="mr-4" htmlFor="tono">Tono de la historia: </label>
+                  <select disabled={loading} value={tono} required onChange={(e) => setTono(e.target.value)} name="tono" id="tono" className="bg-slate-800 px-4 py-2 rounded-lg">
+                    <option value="">Seleccione un tono</option>
+                    <option value="divertido">Divertido</option>
+                    <option value="para dormir">Para dormir</option>
+                    <option value="emocionante">Emocionante</option>
+                  </select>
+                </div>
+                <div className="flex flex-col mb-4">
+                  <label className="mb-2" htmlFor="topic">De que tema quieres tu historia?</label>
+                  <textarea disabled={loading} value={topic} required onChange={(e) => setTopic(e.target.value)}  className="bg-slate-800 p-2 rounded-lg" name="topic" id="topic" rows={3} placeholder="Escribe tu texto aquí" />
+                </div>
+                <input type="submit" disabled={loading} value={`${loading ? "Cargando..." : "Generar Historia"}`} className="bg-sky-600 hover:bg-sky-900 py-2 px-4 text-white p-2 rounded-lg cursor-pointer" />
+              </form>
+        </div>
+        { result !== "" && (
+            <div className="result-section">
+              <h2>Historia</h2>
+              <p>{result}</p>
+            </div>
+        )}
+    </>  
+  )
+}
+
+export default Form
